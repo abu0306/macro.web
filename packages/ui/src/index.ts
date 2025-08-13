1c661f2fceffec85aea3ccd1773941fa0237e35d
abu <2261983469@qq.com>
2025-08-13 10:02:28 +0800

    refactor: remove unused UI components and update exports in index.ts

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,2 @@
-export { Button } from './components/Button';
-export { Card } from './components/Card';
-export { Input } from './components/Input';
-export { Badge } from './components/Badge';
-export { Avatar } from './components/Avatar';
-export { Loading } from './components/Loading';
 export { Header } from './components/Header';
 export { Footer } from './components/Footer';
